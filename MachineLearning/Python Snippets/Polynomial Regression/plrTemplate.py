8b2313d450d17163c6cf4786f11c0082e1924844
DESKTOP-2PF69NR\Harry <harryk87@yahoo.com>
2018-03-28 23:32:05 +0300

    28-03-18

--- MachineLearning/Python Snippets/Polynomial Regression/plrTemplate.py
+++ MachineLearning/Python Snippets/Polynomial Regression/plrTemplate.py
@@ -26,10 +26,36 @@ slrModel = LinearRegression().fit(X,y)
 from sklearn.preprocessing import PolynomialFeatures
 #We use fit_transform in order to tranform the X to its
 #polynomial counterpart
-plrReg = PolynomialFeatures(degree=2)
+plrReg = PolynomialFeatures(degree=4)
 X_poly = plrReg.fit_transform(X)
 #plrReg acts as a tool to create the polynomial X space
 #We fit the mdoel like a normal slr btu with a polynomial X
 plrModel = LinearRegression().fit(X_poly,y)
 
-print("Hello Lina!")
+#Visualizing the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, slrModel.predict(X), color = 'blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+#Visualizing the Polynomial Linear Regression
+#We break the 10 initial classes even more in order
+#to achieve a smoother line
+X_grid = np.arange(min(X), max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, y, color = 'red')
+#We avoid using X_poly because this is a template and
+#we want to be applicable to every X set
+plt.plot(X_grid, plrModel.predict(plrReg.fit_transform(X_grid)), color = 'blue')
+plt.title("Truth or Bluff (Polynomial Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+#Predicting a new result with Linear Regression
+slrModel.predict(6.5)
+
+#Predicting a new result with Polynomial Regression
+plrModel.predict(plrReg.fit_transform(6.5))
